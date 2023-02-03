cc8f75c2a38a8c186a3be8af0a3a64c099735d8c
Ludovic Dumas <ludoloops@pm.me>
2023-02-03 18:03:57 -0500

    Feat: login and display wallet address

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,73 @@
-import React from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import twitterLogo from "./assets/twitter-logo.svg"
+import "./App.css"
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_HANDLE = "_buildspace"
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`
 
 const App = () => {
+  const [currentAccount, setCurrentAccount] = useState(null)
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window
+
+      if (!ethereum) {
+        console.log("Make sure you have MetaMask!")
+        return
+      } else {
+        console.log("We have the ethereum object", ethereum)
+
+        /*
+         * Check if we're authorized to access the user's wallet
+         */
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+
+        /*
+         * User can have multiple authorized accounts, we grab the first one if its there!
+         */
+        if (accounts.length !== 0) {
+          const account = accounts[0]
+          console.log("Found an authorized account:", account)
+          setCurrentAccount(account)
+        } else {
+          console.log("No authorized account found")
+        }
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window
+
+      if (!ethereum) {
+        alert("Get MetaMask!")
+        return
+      }
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      })
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log("Connected", accounts[0])
+      setCurrentAccount(accounts[0])
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
@@ -18,10 +79,20 @@ const App = () => {
               src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
               alt="Monty Python Gif"
             />
+            <button
+              className="cta-button connect-wallet-button"
+              onClick={connectWalletAction}
+            >
+              Connect Wallet To Get Started.
+            </button>
           </div>
         </div>
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <img
+            alt="Twitter Logo"
+            className="twitter-logo"
+            src={twitterLogo}
+          />
           <a
             className="footer-text"
             href={TWITTER_LINK}
@@ -31,7 +102,7 @@ const App = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
